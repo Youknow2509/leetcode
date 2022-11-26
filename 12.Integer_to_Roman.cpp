cef2a768dbaf505e4ebd250b5bf5627e583ab8ad
V-Mac <lytranvinh.2509@gmail.com>
2022-11-26 23:46:00 +0700

    23:45 26/11/22 push v_mac

--- 12.Integer_to_Roman.cpp
+++ 12.Integer_to_Roman.cpp
@@ -10,6 +10,7 @@ class Solution {
 public:
     string intToRoman(int num) {
         string res;
+// code here 
 
         return res;
     }
@@ -25,7 +26,15 @@ int main(){
     mp.insert(pair<int, char>(500,'D'));
     mp.insert(pair<int, char>(1000,'M')); 
 
+    Solution st;
+    
+    int n, x;
+    cin >> n;
 
+    while (n--){
+        cin >> x;
+        cout << st.intToRoman(x) << endl;
+    }
 
     return 0;
 }
