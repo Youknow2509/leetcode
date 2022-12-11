7432588568ab7d1e5ecf221d9fcbad584f8dc196
V <lytranvinh.2590@gmail.com>
2022-12-11 18:17:05 +0700

    loading ...

--- code/26.Remove_Duplicates_from_Sorted_Array.cpp
+++ code/26.Remove_Duplicates_from_Sorted_Array.cpp
@@ -5,29 +5,45 @@ using namespace std;
 
 class Solution {
 public:
-    int removeDuplicates(vector<int>& nums) {
-        
+    int removeDuplicates(vector<int> nums) {
+
+        int n = nums.size(), res = 0, i = 0, j = 1;
+        solve(nums, i, j, res);
+        res = n - res;
+        return res;
+    }
+    void solve(vector<int> nums, int i, int j, int &res){
+        if(i > nums.size() - 1) return;
+        if(nums[i] == nums[j]){
+            res++;
+            solve(nums, i, j++, res);
+        }
+        else solve(nums, j, j++, res);
     }
 };
-
+// ex input:  nums = [0,0,1,1,1,2,2,3,3,4]
+//     ptr            0 1 2 3 4 5 6 7 8 9
 int main(){
 
     Solution st;
-    int n, arr, size_arr;
+    int n, size_arr, x;
 
     cout << "Test case: "; cin >> n;
     cout << endl;
 
     while(n--){
+        vector<int> arr;
         cout << "Size array: "; cin >> size_arr;
-        int arr[size_arr];
         cout << "Input array: ";
 
-        for (int i = 0; i < size_arr; i++)
-            cin >> arr[i];
-
-        
-    }
+        for (int i = 0; i < size_arr; i++){
+            cin >> x;
+            arr.push_back(x);
+        }
+        cout << endl;
+        cout << "Output: " << st.removeDuplicates(arr);
+        cout << endl;
+        }
 
     return 0;
 }
