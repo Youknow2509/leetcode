f63480b7a421b60e51b0507956380ba270af659c
V <lytranvinh.2509@gmail.com>
2023-05-01 00:03:15 +0700

    v push 141.Linked_List...

---
+++ code/141.Linked_List_Cycle.cpp
@@ -0,0 +1,32 @@
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+};
+
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        
+    }
+};
+
+
+
+int main(){
+
+    #ifndef ONLINE_JUDGE
+    freopen("Show_screen/INP.TXT", "r", stdin);
+	freopen("Show_screen/OUT.TXT", "w", stdout);
+    #endif
+    ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+    
+    
+
+    return 0;
+}
